6cd347eedf2fdaaf7fdc7611be99a1743ebda8ee
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-03-08 13:01:30 -0500

    Created PropTypes

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
 
 const Tabs = props => {
   return (
@@ -25,4 +26,7 @@ const Tabs = props => {
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.array.isRequired
+}
 export default Tabs;
