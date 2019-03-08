b27dd46358c813c9bc4841292d52dce6e57bd73b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-03-08 12:13:08 -0500

    Cards are rendered

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,9 +6,17 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+          {props.cards.map((card, i) => {
+            return (
+            <Card 
+              card={card}
+              key={i}
+            />
+            );
+          })}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
